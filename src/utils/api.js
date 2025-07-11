b4a43c787a289b7e50fc3d8820eb4d0ef4118263
yoohmoon <121086010+yoohmoon@users.noreply.github.com>
2025-07-11 15:18:47 +0900

    redirets proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: `${process.env.REACT_APP_BACKEND_URL_PROXY}/api`,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 /**
@@ -11,22 +11,22 @@ const api = axios.create({
  */
 api.interceptors.request.use(
   (request) => {
-    console.log("Starting Request", request);
+    console.log('Starting Request', request);
     return request;
   },
   function (error) {
-    console.log("REQUEST ERROR", error);
+    console.log('REQUEST ERROR', error);
   }
 );
 
 api.interceptors.response.use(
   (response) => {
-    console.log("Response:", response);
+    console.log('Response:', response);
     return response;
   },
   function (error) {
     error = error.response.data;
-    console.log("RESPONSE ERROR", error);
+    console.log('RESPONSE ERROR', error);
     return Promise.reject(error);
   }
 );
